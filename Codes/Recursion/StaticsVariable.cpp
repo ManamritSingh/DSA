064e4f41beea46554780f0c284513c19669dc46c
Manamrit Singh <manamritsingh426@gmail.com>
2023-06-23 01:07:02 +0530

    Understanding static variables

---
+++ Codes/Recursion/StaticsVariable.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int fun(int n){
+
+    if (n>0)
+    {
+        return fun(n-1)+n;
+    }
+    return 0;
+}
+
+int fun1(int n){
+    static int x=0;
+    if (n>0)
+    {
+        x++;
+        return fun1(n-1)+x;
+    }
+    return 0;
+}
+
+int main(){
+    int a =5;
+    int r = fun(a);
+    int k = fun1(a);
+    cout<<r<<endl;
+    cout<<k<<endl;
+    return 0;
+}
