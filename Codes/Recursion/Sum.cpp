bee69e3a8aaf56a98d51c27db316acf9d801d611
Manamrit Singh <manamritsingh426@gmail.com>
2023-06-25 01:42:55 +0530

    Sum of n natural numbers by 3 methods

---
+++ Codes/Recursion/Sum.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int recursion(int n){  // O(n) time but makes stack so takes a lot of space
+    if (n==0){
+        return 0;
+    }
+    else{
+        return (recursion(n-1)+n);
+    }
+}
+
+int loop(int n){  // O(n) time ans space is just the 3 variables it makes
+int s =0;
+for (int i = 0; i < n; i++)
+{
+    s = s+1; 
+}
+return s;
+}
+
+int formula(int n){  // best O(1) time and takes just 2 variables
+    int sum = 0;
+
+    sum = n*(n+1)/2;
+
+    return sum;
+}
+
+int main(){
+    cout<<recursion(10)<<endl;
+    cout<<loop(10)<<endl;
+    cout<<formula(10)<<endl;
+    return 0;
+}
