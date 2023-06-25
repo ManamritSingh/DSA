3a6210862deeea3b4986137c9063658b9eac19e0
Manamrit Singh <manamritsingh426@gmail.com>
2023-06-26 01:32:23 +0530

    Accessing an array element

---
+++ Data structures/Arrays/AcessingAnArray.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int A[]={1,2,3,4,5};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<A[i]<<endl;
+    }
+    
+
+    cout<<A[3]<<endl;
+    cout<<3[A]<<endl;
+    cout<<*(A+3)<<endl;
+    return 0;
+}
