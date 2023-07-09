50866ea8c15c66e7500ceb2804b33ee2457a3b31
Manamrit Singh <manamritsingh426@gmail.com>
2023-07-10 00:00:24 +0530

    length of a string

---
+++ Data structures/Strings/length.cpp
@@ -0,0 +1,17 @@
+// this code is for finding the length of a string
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    char s[]="Manamrit";
+    int i;
+
+    for (i = 0; s[i]!='\0'; i++)
+    {
+        /* code */
+    }
+    cout<<"Length is "<<i;
+    
+    return 0;
+}
