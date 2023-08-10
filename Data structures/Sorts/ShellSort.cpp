dda54a20efb067212984a72af2ad2dcffdb92249
Manamrit Singh <manamritsingh426@gmail.com>
2023-08-11 02:26:46 +0530

    Shell Sort

---
+++ Data structures/Sorts/ShellSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void shellSort(int A[] , int n){
+    for(int gap = n/2 ; gap >=1 ; gap/=2){
+        for (int j = gap; j < n; j++)
+        {
+            int temp = A[j];
+            int i = j-gap;
+            while (i>= 0 && A[i] > temp)
+            {
+                A[i+gap]=A[i];
+                i-=gap;
+            }
+            A[i+gap]=temp;
+        }
+        
+    }
+}
+
+int main() {
+    int A[] = {11, 13, 7, 12, 16, 9, 24, 5, 10, 3};
+    int n = sizeof(A)/sizeof(A[0]);
+
+    shellSort(A , n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<A[i]<<" ";
+    }
+    
+    
+    return 0;
+}
